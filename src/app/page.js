b42a6903792a1157214128330337e85f2cc83aff
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2023-07-25 17:11:03 +0600

    swipe-images-mechanism-and-magnification

--- src/app/page.js
+++ src/app/page.js
@@ -29,7 +29,59 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between lg:p-24 mt-4">
       <div className="lg:flex-row flex flex-col items-center justify-center w-full gap-5 lg:px-12 px-2">
         <div className="lg:w-1/2 w-full h-12 bg-slate-300">
-       
+        <div className="relative">
+              <AiOutlineLeft
+                onClick={handlePrevSlide}
+                className="absolute z-10   left-0 m-auto text-5xl inset-y-1/2 cursor-pointer text-gray-400 "
+              />
+              <div className="w-full h-[60vh] flex overflow-hidden relative m-auto">
+                <Swipe
+                  onSwipeLeft={handleNextSlide}
+                  onSwipeRight={handlePrevSlide}
+                  className="relative  w-full h-full"
+                >
+                  {images.map((image, index) => {
+                    if (index === currentSlide) {
+                      return (
+                        <InnerImageZoom
+                          key={image.id}
+                          src={image.src}
+                          zoomSrc={image.src}
+                          image={image}
+                          layout="fill"
+                          objectFit="contain"
+                          className="animate-fadeIn"
+                          alt={image.id}
+                         
+                        />
+                      );
+                    }
+                  })}
+                </Swipe>
+              </div>
+              <AiOutlineRight
+                onClick={handleNextSlide}
+                className="absolute z-auto right-0 m-auto text-5xl inset-y-1/2 cursor-pointer text-gray-400"
+              />
+
+              <div className="relative flex justify-center p-2">
+                {images.map((_, index) => {
+                  return (
+                    <div
+                      className={
+                        index === currentSlide
+                          ? "h-4 w-4 bg-gray-700 rounded-full mx-2 mb-2 cursor-pointer"
+                          : "h-4 w-4 bg-gray-300 rounded-full mx-2 mb-2 cursor-pointer"
+                      }
+                      key={index}
+                      onClick={() => {
+                        setCurrentSlide(index);
+                      }}
+                    />
+                  );
+                })}
+              </div>
+            </div>
         </div>
         <div className="lg:w-1/2 w-full h-12 bg-slate-500"></div>
       </div>
