48710346b2ab3563dd44b3f0393beedae5462d32
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2023-07-26 20:51:31 +0600

    React Hook usePathname is called in function categoryLayout that is neither a React function component nor a custom React Hook function. React component names must start with an uppercase letter.-BUG-FIXED

--- src/app/category/layout.js
+++ src/app/category/layout.js
@@ -1,7 +1,7 @@
 "use client"
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-export default function categoryLayout({ children }) {
+export default function CategoryLayout({ children }) {
     const path =usePathname()
     return (
       <div>
