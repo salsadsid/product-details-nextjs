749857768be9fd55201b39feaaf7762786669705
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2023-07-26 20:47:19 +0600

    breadcrumb-added

--- src/app/category/page.js
+++ src/app/category/page.js
@@ -1,27 +1,33 @@
-"use client"
-import ProductCard from '@/components/productCard/ProductCard';
-import React, { useEffect, useState } from 'react'
+"use client";
+import ProductCard from "@/components/productCard/ProductCard";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 const Categories = () => {
-    const [allProducts, setAllProducts] = useState([]);
-    const fetchProducts = async () => {
-      const response = await fetch(`/api/product`);
-      const data = await response.json();
-      console.log(data)
-      setAllProducts(data);
-    };
-  
-    useEffect(() => {
-        fetchProducts();
-    }, []);
-    console.log(allProducts);
-  return (
-    <div className='flex justify-center items-center gap-5 mt-8 flex-wrap'>
-    {
-        allProducts?.map(product=><ProductCard  key={product._id} product={product}></ProductCard>)
-    }
-   </div>
-  )
-}
+  const [allProducts, setAllProducts] = useState([]);
+  const path= usePathname()
+  const fetchProducts = async () => {
+    const response = await fetch(`/api/product`);
+    const data = await response.json();
+    console.log(data);
+    setAllProducts(data);
+  };
 
-export default Categories
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  console.log(allProducts);
+  return (
+    <div>
+      
+      <div className="flex justify-center items-center gap-5 mt-8 flex-wrap">
+        {allProducts?.map((product) => (
+          <ProductCard key={product._id} product={product}></ProductCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
